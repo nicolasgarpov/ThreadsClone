4a1dd8b17bb350071f0f858e7fa7ed5b9b4f300f
nicolasgarpov <nicolasgarpov64@gmail.com>
2025-05-26 00:45:15 -0500

    Adjust the DarkTheme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,22 @@
 import { Slot } from "expo-router";
 
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+
+const myTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: "white",
+    card: "#101010",
+  },
+};
+
 export default function RootLayout() {
   console.log("RootLayout rendered");
 
-  return <Slot />;
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
