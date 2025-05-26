9aa1713af08e990579468f435fb00babab1f0792
nicolasgarpov <nicolasgarpov64@gmail.com>
2025-05-26 18:57:41 -0500

    Path Alias Config

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text className="text-3xl font-bold text-red-500 p-2 ">Feed</Text>
+    <View className="flex-1 p-4">
+      <Text className="text-3xl font-bold text-white p-2 ">Hello Feed</Text>
     </View>
   );
 }
