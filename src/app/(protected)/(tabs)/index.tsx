a3911f8e790a367fa3aad6afa85c45369860ac1d
nicolasgarpov <nicolasgarpov64@gmail.com>
2025-05-30 13:32:38 -0500

    Auth UI: Login and signup sreens

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -9,9 +9,18 @@ export default function HomeScreen() {
       data={Posts}
       renderItem={({ item }) => <PostListItem post={item} />}
       ListHeaderComponent={() => (
-        <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
-          New Post
-        </Link>
+        <>
+          {" "}
+          <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+            New Post
+          </Link>
+          <Link
+            href="/login"
+            className="text-blue-500 p-4 text-center text-3xl"
+          >
+            Login
+          </Link>
+        </>
       )}
     />
   );
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import PostListItem from "@/components/PostListItem";
 import { Posts } from "@/data";
+import { Link } from "expo-router";
 import { FlatList } from "react-native";
 
 export default function HomeScreen() {
@@ -7,6 +8,16 @@ export default function HomeScreen() {
     <FlatList
       data={Posts}
       renderItem={({ item }) => <PostListItem post={item} />}
+      ListHeaderComponent={() => (
+        <>
+          <Link href="/new" className=" p-4 text-center text-3xl">
+            New Post
+          </Link>
+          <Link href="/new" className=" p-4 text-center text-3xl">
+            New Post
+          </Link>
+        </>
+      )}
     />
   );
 }
